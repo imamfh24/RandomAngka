20c7c6c9a6402673d881a2137d747852c7adc8cb
IFA <imamfh24@gmail.com>
2020-02-28 23:47:30 +0700

    inisialisasi 4

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ btnRandom.addEventListener('click',function(e){
     const nilai = document.getElementById('hasilRandom')
     nilai.innerHTML = random;
 
-    // if (!liff.isInClient()) {
-    //     sendAlertIfNotInClient();
-    // } else {
-    //     liff.sendMessages([{
-    //         'type': 'text',
-    //         'text': "Kamu telah melakukan random angka yang bernilai : " + random + ""
-    //     }]).then(function () {
-    //         window.alert('Kamu telah melakukan random angka');
-    //     }).catch(function (error) {
-    //         window.alert('What happend?');
-    //     });
-    // }
+    if (!liff.isInClient()) {
+        sendAlertIfNotInClient();
+    } else {
+        liff.sendMessages([{
+            'type': 'text',
+            'text': "Kamu telah melakukan random angka yang bernilai : " + random + ""
+        }]).then(function () {
+            window.alert('Kamu telah melakukan random angka');
+        }).catch(function (error) {
+            window.alert('What happend?');
+        });
+    }
 });
