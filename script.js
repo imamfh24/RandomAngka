b465f48fdfa60a907e7cd8db172fd6f6e6bda095
IFA <imamfh24@gmail.com>
2020-02-28 23:37:23 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const inputAngka = document.getElementById('batasAngka');
+
+inputAngka.oninput = setBatas;
+function setBatas (){
+    const batasAngka = inputAngka.value;
+    return batasAngka;
+}
+
+const btnRandom = document.getElementById('randomAngka');
+btnRandom.addEventListener('click',function(e){
+    e.preventDefault();
+    const batasan = setBatas();
+    const random = Math.round(Math.random() * batasan);
+    
+    const nilai = document.getElementById('hasilRandom')
+    nilai.innerHTML = random;
+
+    // if (!liff.isInClient()) {
+    //     sendAlertIfNotInClient();
+    // } else {
+    //     liff.sendMessages([{
+    //         'type': 'text',
+    //         'text': "Kamu telah melakukan random angka yang bernilai : " + random + ""
+    //     }]).then(function () {
+    //         window.alert('Kamu telah melakukan random angka');
+    //     }).catch(function (error) {
+    //         window.alert('What happend?');
+    //     });
+    // }
+});
